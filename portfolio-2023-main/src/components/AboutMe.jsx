f12599c2cd92c64cdeca482a6fcbc2618aeb1d73
Shashank Yadav <97789487+Shashank2104125@users.noreply.github.com>
2023-06-18 18:45:11 +0530

    initial Commit

---
+++ portfolio-2023-main/src/components/AboutMe.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
+
+import Wrapper from "./Wrapper";
+import flag from "../assets/indian-flag.png";
+import pe1 from "../assets/sec-2-p-e-1.png";
+import pe2 from "../assets/sec-2-p-e-2.png";
+import Div from "./Div";
+
+const AboutMe = () => {
+    const { scrollY } = useScroll();
+    const y1 = useTransform(scrollY, [0, 500], [100, 0], { clamp: false });
+    const y2 = useTransform(scrollY, [0, 500], [0, -200], { clamp: false });
+    return (
+        <div
+            id="about"
+            className="w-full py-[50px] md:py-[100px] bg-white text-black relative overflow-hidden"
+        >
+            {/* BACKGROUND ELEMENTS START */}
+            <span className="sec-2-bg-gradient" />
+            <motion.img className="sec-2-p-e-1" style={{ y: y1 }} src={pe1} />
+            <motion.img className="sec-2-p-e-2" style={{ y: y2 }} src={pe2} />
+            {/* BACKGROUND ELEMENTS END */}
+
+            <Wrapper>
+                {/* HEADING START */}
+                <Div className="text-[40px] md:text-[90px] 1xl:text-[120px] leading-[52px] md:leading-[95px] 2xl:leading-[123px] font-oswald uppercase mb-[25px] md:mb-[40px] 2xl:mb-[60px] flex flex-col relative">
+                    <span>About Me</span>
+                    {/* <span className="flex items-center gap-2">
+                        <span>Based</span>
+                        <img
+                            src={flag}
+                            alt=""
+                            className="w-[70px] md:w-[150px] 2xl:w-[190px] block mt-1 md:mt-2"
+                        />
+                        <span>In India</span>
+                    </span> */}
+                </Div>
+                {/* HEADING END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                Hello! I'm Shashank Yadav, a passionate web developer with a keen eye for design and a love for 
+                clean, efficient code. Specializing in front-end development, I create visually stunning websites 
+                and applications. I thrive on collaboration, working closely with clients to transform their visions 
+                into reality. With a strong foundation in HTML, CSS, and JavaScript, I stay up-to-date with the latest
+                trends and technologies to deliver engaging user experiences. Let's connect and bring your digital 
+                dreams to life!
+                </Div>
+                {/* PARAGRAPH END */}
+
+                {/* PARAGRAPH START */}
+                {/* <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it
+                </Div> */}
+                {/* PARAGRAPH END */}
+
+                {/* PARAGRAPH START */}
+                {/* <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] relative">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled itLorem Ipsum is
+                    simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it
+                </Div> */}
+                {/* PARAGRAPH END */}
+            </Wrapper>
+        </div>
+    );
+};
+
+export default AboutMe;
---
+++ portfolio-2023-main/src/data.js
@@ -0,0 +1,82 @@
+import p1 from "./assets/project-1.png";
+import p2 from "./assets/project-2.png";
+import p3 from "./assets/project-3.png";
+import p4 from "./assets/project-4.png";
+import p5 from "./assets/project-5.png";
+import p6 from "./assets/project-6.png";
+
+export const filters = [
+    {
+        name: "All",
+        id: "all",
+    },
+    {
+        name: "Team Project",
+        id: "dashboard",
+    },
+    // {
+    //     name: "Mobile App",
+    //     id: "mobile-app",
+    // },
+    {
+        name: "Personal Project",
+        id: "website",
+    },
+];
+
+export const projects = [
+    {
+        name: "E-complain student register",
+        image: p1,
+        tags: ["dashboard", "dashboard"],
+    },
+    {
+        name: "College Hackathon",
+        image: p2,
+        tags: ["dashboard", "dashboard"],
+    },
+    {
+        name: "Your one stop opportunity",
+        image: p3,
+        tags:["dashboard", "dashboard"],
+    },
+    {
+        name: "Nit Patna Official Website",
+        image: p4,
+        // tags: ["mobile-app"],
+        tags:["dashboard", "dashboard"],
+    },
+    {
+        name: "The English Tea Ceremony",
+        image: p5,
+        tags: ["website"],
+    },
+    {
+        name: "Personal Portfolio",
+        image: p6,
+        tags: ["website"],
+    },
+];
+
+export const achievements = [
+    {
+        id: 1,
+        year: 5,
+        description: "SUCCESSFULLY<br />COMPLETED<br />PROJECTS",
+    },
+    {
+        id: 2,
+        year: 1,
+        description: "YEARS OF<br />EXPERIENCE",
+    },
+    {
+        id: 3,
+        year: 10,
+        description: "TECHNOLOGIES",
+    },
+    // {
+    //     id: 4,
+    //     year: 70,
+    //     description: "COMMUNITY<br />POSTS",
+    // },
+];
